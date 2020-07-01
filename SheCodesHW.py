c0426538dd89e8ec27cb824218b2e907c1712ab9
imogeneweatherly <67363143+imogeneweatherly@users.noreply.github.com>
2020-07-01 14:40:23 +0800

    Add files via upload

---
+++ SheCodesHW.py
@@ -0,0 +1,55 @@
+# Question 1
+
+x = 3 
+y = 9
+
+print(x+y)
+
+a = -3
+b = 9
+
+print (a+b)
+
+c = 3.0
+d = -9 
+
+print (c+d)
+
+# Question 2
+
+e = 3
+f = 9 
+
+print (e*f)
+
+g = -3
+h = 9
+
+print (g*h)
+
+i = 3.0
+j = -9
+
+print (i*j)
+
+# # Question 3 
+# Convert km into m
+
+kilometers = float(input("Enter value in kilometers: "))
+
+meter = kilometers *1000.0 
+centimeter = kilometers *10000.0
+
+# Convert km into mm 
+
+print('%0.2f kilometers is equal to %0.2f meters' %(kilometers,meter))
+
+print('%0.2f kilometers is equal to %0.2f centimetres' %(kilometers,centimeter))
+
+
+# Question 4 
+
+NAME = str(input("Enter the name"))
+Height = str(input("Enter height"))
+print(NAME, "is", Height, "cm tall." )
+
---
+++ SheCodesHW1.py
@@ -0,0 +1,12 @@
+
+
+True = moths_in_house
+False = no_moths_in_house
+
+if moths_in_house == True:
+    print("Get the moths!")
+else:
+    if no_moths_in_house
+    print("No threats detected")
+
+
